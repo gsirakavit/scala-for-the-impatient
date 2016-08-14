e72ab91afad8616f46cd93cdbe3d4d17989fb804
Glen Sirakavit <gsirakavit@hotmail.com>
2016-08-14 14:31:53 -0600

    Made Ch02 Ex10 recursive as task stated

--- src/main/scala/com/basile/scala/ch02/Ex10.scala
+++ src/main/scala/com/basile/scala/ch02/Ex10.scala
@@ -15,9 +15,9 @@ import scala.math.pow
 object Ex10 extends App {
 
   def compute(x: Double, n: Int): Double = {
-    if (n > 0 && n % 2 == 0) pow( pow( x, n / 2 ), 2 )
-    else if (n > 0 && n % 2 != 0) x * pow( x, n - 1 )
-    else if (n < 0) 1 / pow( x, -n )
+    if (n > 0 && n % 2 == 0) pow( compute(x, n/2), 2)
+    else if (n > 0 && n % 2 != 0) x * compute(x, n - 1)
+    else if (n < 0) 1 / compute(x, -n)
     else 1
   }
 
